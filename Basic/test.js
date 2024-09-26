e096f29cca6a89039def9753a9270d2a44b98acb
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-09-26 08:53:29 +0000

    Keyword let,var,const

---
+++ Basic/test.js
@@ -0,0 +1,15 @@
+const accountId = 420;
+let AccountEmail = "abc@gmail.com";
+var AccountPassword= "abc";
+AccountEmail="ac@gamil.c";
+AccountPassword = "234";
+AccountCity= "Delhi";
+// accountId= 45 * not allowed bcz we can't assign value to const keyword*
+// please dont use var bcz of scope
+*/
+use only let keyword for variable decelaration.
+*/
+console.log(accountId);
+console.log(AccountEmail);
+ console.table([accountId,AccountEmail,AccountPassword,AccountCity]);
+
