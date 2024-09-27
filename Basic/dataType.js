e01e598836fd604d93290bb9d85a81c3d2c28b62
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-09-27 08:21:13 +0000

    dataType

---
+++ Basic/dataType.js
@@ -0,0 +1,26 @@
+"use strict";  // treat Js code as a newer version
+//alert(2+2)
+console.log(3+6);
+let name = "raj";// string
+let age = 25;// 
+let state ="null";
+let isLoggedIn= false;
+console.log(state);
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof age);
+
+console.log(typeof name);
+console.log(typeof null);  // type=object
+
+//number -> 2to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null => stand alone value
+// undefined=>
+    //symbol => unique
+
+
+// boject 
