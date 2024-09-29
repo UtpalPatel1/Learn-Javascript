c42f800b9c3a911d258e9e73d6bf257e3a103542
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-09-29 19:26:59 +0000

    data type summery

---
+++ Basic/DataType_summery.js
@@ -0,0 +1,46 @@
+// primitive =>
+    // 7 types:: Number,String,Boolean,null,undefined,symbol(for making it unique), bigint(bigger values)
+
+//Reference type::(Non-primitive)
+//Array,Object,Function
+
+//Is js is static or dynamic type language.
+
+//const score = 100; //number
+const scoreNew = 100.3; // Number
+
+const isLoggedIn = false;
+const outsidetemp = null;
+let userEmail ; // undefined
+
+const id =Symbol('123');
+const newId = Symbol('123');
+console.log(id===newId);
+console.log(id);
+console.log(newId);
+
+
+//const bigNumber = 9856234785n;
+console.log(typeof bigNumber);
+
+const hreos =["shaktiman","krrish"];
+
+ let myObj={
+    name:"utpal",
+    agr:22,
+ }
+// things under {} is object
+
+//Function
+const myFunction = function(){
+    console.log("function");
+}
+console.log(typeof hreos);
+console.log(typeof myObj);
+console.log(typeof score)
+
+
+//return type of non-primitive datatype is "function" but for function is called "object function"
+
+
+
