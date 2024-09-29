6c5b90817620a17b9977598e0bd4a3ad11b767bb
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-09-29 18:01:43 +0000

    conversion boolean,number ,string

---
+++ Basic/Conversion.js
@@ -0,0 +1,31 @@
+let score = 33;
+let age = "33abc"
+let name1 = "rahul";// try with null,undefined also to check output
+console.log(typeof score);
+console.log(typeof (score));
+console.log(typeof age);
+
+let valueInNumber = Number(age);
+let name2 = Number(name1);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(name2);
+
+//"33">>33
+// "33abc">> nan
+//true=1,false=0
+
+let isLoggedIn="ered";
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn);
+
+// 1= true
+//0= false
+//""= false
+//"utpal"=true
+ let stringNum = 33 // try with null, blank value, undefined, string
+ let conNum = String(stringNum);
+ console.log(typeof conNum);
+ console.log(conNum);
+
