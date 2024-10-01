e33752f6920178bc2d0e99ff7e47b83191f28721
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-10-01 07:37:23 +0000

    Operations like string , number combination

---
+++ Basic/Operation.js
@@ -0,0 +1,28 @@
+let value = 3;
+let negValue= -value
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str = "utpal"
+let str2 = " hello"
+let str3 = str + str2;
+console.log(str3);
+console.log("1" +2);
+console.log(1 +"2");
+
+console.log("1" +"2");
+console.log("1" +2 +2);
+console.log(1 +2 +"8");
+
+
+
+
+
+
+
+
