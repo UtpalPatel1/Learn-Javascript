827b0872879b15859ec8709628e26c89c6fe3df0
Utpal patel <46716790+UtpalPatel1@users.noreply.github.com>
2024-10-04 07:55:41 +0000

    number , math value

---
+++ Basic/number.js
@@ -0,0 +1,30 @@
+// const num =400;
+// console.log(num);
+
+// const score =new Number(402)
+// console.log(score);
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+// const value = 123.498
+// console.log(value.toPrecision(4));
+
+// const cost = 1000000;
+// console.log(cost.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(25.6));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+//use floor,ceil with Math.random method
+
+
+
+
